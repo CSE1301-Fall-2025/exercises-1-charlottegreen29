6460b7fb5f398c5307eab0af56c8ee32cea1cf58
charlottegreen29 <c.j.green@wustl.edu>
2025-09-05 11:20:52 -0500

    done

--- src/module1/_3expressions/E01VariableAssignment.java
+++ src/module1/_3expressions/E01VariableAssignment.java
@@ -9,11 +9,14 @@ public class E01VariableAssignment {
 		int y = 2;
 		System.out.println(x);
 		System.out.println(y);
+		System.out.println();
 		x = y;
 		System.out.println(x);
 		System.out.println(y);
+		System.out.println();
 		y = 5;
 		System.out.println(x);
 		System.out.println(y);
+		System.out.println();
 	}
 }
