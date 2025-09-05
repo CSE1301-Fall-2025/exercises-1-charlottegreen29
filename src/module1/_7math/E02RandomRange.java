24a7f4a1c2678d2086da13decedddce189bf8e41
charlottegreen29 <c.j.green@wustl.edu>
2025-09-05 14:04:44 -0500

    done

--- src/module1/_7math/E02RandomRange.java
+++ src/module1/_7math/E02RandomRange.java
@@ -5,6 +5,15 @@ public class E02RandomRange {
 	//Exercise 2: Run this code several times and notice how the
 	//random values have a different range.
 	public static void main(String[] args) {
-		System.out.println((int) (Math.random() * 10));
+		System.out.println((int) (Math.random() * 10 + 1)); //by adding 1, I can get it to do 1 to 10 inclusive!
+		System.out.println((int) (Math.random() * 10 + 1));  //idk how to do loops in java yet so imma copy and paste so i don't have to keep running it
+		System.out.println((int) (Math.random() * 10 + 1));
+		System.out.println((int) (Math.random() * 10 + 1));
+		System.out.println((int) (Math.random() * 10 + 1));
+		System.out.println((int) (Math.random() * 10 + 1));
+		System.out.println((int) (Math.random() * 10 + 1));
+		System.out.println((int) (Math.random() * 10 + 1));
+		System.out.println((int) (Math.random() * 10 + 1));
+		System.out.println((int) (Math.random() * 10 + 1));
 	}
 }
